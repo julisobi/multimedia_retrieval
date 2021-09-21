62ee659ad6c449f8568e98ec256e738d646a8358
Julita Sobiczewska <44268786+julisobi@users.noreply.github.com>
2021-09-21 20:52:06 +0200

    Add files via upload
    create output

--- read_data.py
+++ read_data.py
@@ -1,7 +1,10 @@
 import trimesh
+import os
+import glob
 
 FILE = 'm107.off'
 FILE2 = 'car_wheel_cap.ply'
+DIR = 'LabeledDB_new'
 trimesh.util.attach_to_log()
 
 
@@ -10,4 +13,19 @@ def view_3d(file):
     mesh.show()
 
 
-view_3d(FILE)
+output = []
+i = 0
+for dir in os.listdir(DIR):
+    for filename in glob.iglob(f'{DIR}/{dir}/*.off'):
+        with open(filename) as f:
+            second_line = f.readlines()[1]
+            num_faces = second_line.split()[1]
+            num_vert = second_line.split()[0]
+        new_dict = {"shape_class": str(dir), "num_verticles": num_vert, "num_faces": num_faces}
+        output.append(new_dict)
+        i += 1
+print(f"Number of 3D objects in dataset: {i}")
+
+
+# print(output)
+# view_3d(FILE)
