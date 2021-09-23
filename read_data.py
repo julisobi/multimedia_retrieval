d4057ee6739a181c8a6f3f2c04035fb7e3d664ed
Julita Sobiczewska <44268786+julisobi@users.noreply.github.com>
2021-09-23 19:12:39 +0200

    Add files via upload

--- read_data.py
+++ read_data.py
@@ -1,8 +1,9 @@
 import trimesh
 import os
 import glob
+import pandas as pd
 from tqdm import tqdm
-import matplotlib.pyplot as plt
+from openpyxl.workbook import Workbook
 
 FILE = 'm107.off'
 FILE2 = 'car_wheel_cap.ply'
@@ -15,72 +16,58 @@ def view_3d(file):
     mesh.show()
 
 
-output = []
-i = 0
+def save_ouput(fold):
+    output = []
+    i = 0
+    vertice_nums = []
+    category_nums = []
+    faces_nums = []
 
-# The following three variables are for the histograms
-vertice_nums = []
-category_nums = []
-faces_nums = []
+    for dir in tqdm(os.listdir(fold)):
+        for filename in glob.iglob(f'{fold}/{dir}/*.off'):
+            triangles, quads = False, False
+            type_of_faces = ''
+            with open(filename) as f:
+                num_line = 1
+                for line in f.readlines():
+                    if num_line == 2:
+                        second_line = line
+                    if line[0] == '3':
+                        triangles = True
+                    elif line[0] == '4':
+                        quads = True
+                    num_line += 1
+                if triangles and quads:
+                    type_of_faces = 'mix'
+                elif triangles:
+                    type_of_faces = 'triangles'
+                elif quads:
+                    type_of_faces = 'quads'
+                num_faces = second_line.split()[1]
+                num_vert = second_line.split()[0]
 
-for dir in tqdm(os.listdir(DIR)):
-    for filename in glob.iglob(f'{DIR}/{dir}/*.off'):
-        triangles = False
-        quads = False
-        type_of_faces = ''
-        with open(filename) as f:
-            num_line = 1
-            for line in f.readlines():
-                if num_line == 2:
-                    second_line = line
-                if line[0] == '3':
-                    triangles = True
-                elif line[0] == '4':
-                    quads = True
-                num_line += 1
-            if triangles and quads:
-                type_of_faces = 'mix'
-            elif triangles:
-                type_of_faces = 'triangles'
-            elif quads:
-                type_of_faces = 'quads'
-            num_faces = second_line.split()[1]
-            num_vert = second_line.split()[0]
+                # Append the necessary info to variables for histograms
+                vertice_nums.append(int(num_vert))
+                category_nums.append(str(dir))
+                faces_nums.append(int(num_faces))
 
-            # Append the necessary info to variables for histograms
-            vertice_nums.append(int(num_vert))
-            category_nums.append(str(dir))
-            faces_nums.append(int(num_faces))
+            mesh = trimesh.load(filename, force='mesh')
+            new_dict = {"shape_class": str(dir), "num_verticles": int(num_vert), "num_faces": int(num_faces),
+                        "faces_type": str(type_of_faces), "axis_bound_box": mesh.bounding_box,
+                        "bound_box": mesh.bounding_box_oriented}
+            output.append(new_dict)
+            i += 1
+    print(f"Number of 3D objects in dataset: {i}")
+    return output
 
-        mesh = trimesh.load(filename, force='mesh')
-        new_dict = {"shape_class": str(dir), "num_verticles": int(num_vert), "num_faces": int(num_faces),
-                    "faces_type": str(type_of_faces), "axis_bound_box": mesh.bounding_box, "bound_box": mesh.bounding_box_oriented }
-        output.append(new_dict)
-        i += 1
-print(f"Number of 3D objects in dataset: {i}")
-print(output)
 
-# Distribution of amount of vertices before preprocessing
-plt.hist(vertice_nums, rwidth=0.95, bins=15)
-plt.title('Distribution of amount of vertices')
-plt.ylabel('frequency')
-plt.xlabel('# of vertices')
-plt.show()
+def save_excel(folder):
+    out = save_ouput(folder)
+    df = pd.DataFrame.from_dict(out)
+    df.to_excel('filter.xlsx')
 
-# Distribution of different categories before preprocessing
-plt.hist(category_nums, rwidth=0.95, bins=19)
-plt.xticks(rotation=90)
-plt.title('Distribution of categories')
-plt.ylabel('frequency')
-plt.xlabel('category')
-plt.show()
 
-# Distribution of amount of faces before preprocessing
-plt.hist(faces_nums, rwidth=0.95, bins=15)
-plt.title('Distribution of amount of faces')
-plt.ylabel('frequency')
-plt.xlabel('# of faces')
-plt.show()
+save_excel(DIR)
 
 
 # print(output)
