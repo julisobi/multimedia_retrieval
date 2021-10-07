c04c7396e7836fc9d8006d3efa035e090fb33458
Julita Sobiczewska <44268786+julisobi@users.noreply.github.com>
2021-10-07 19:35:10 +0200

    Add files via upload
    preprocessing updated

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pymeshlab as ml
 import os
 import glob
+import matplotlib.pyplot as plt
 from shutil import copyfile
 from tqdm import tqdm
 
@@ -18,16 +19,17 @@ def save_output(fold):
             with open(filename) as f:
                 line = f.readlines()[1]
                 num_vert = line.split()[0]
-                if int(num_vert) > 10000:
+                if int(num_vert) > 8000:
                     i += 1
                     copyfile(filename, f'{NEW_DIR}/subs_{dir}_{filebasename}')
+                    print(filename)
                 else:
                     copyfile(filename, f'{NEW_DIR}/{dir}_{filebasename}')
     print(f"{i} files ")
 
 
-def remesh(dir):
-    TARGET = 10000
+def subsampling(dir):
+    TARGET = 8000
     num_faces = 100 + 2 * TARGET
     os.chdir(dir)
     for file in tqdm(glob.iglob('subs_*.off')):
@@ -42,5 +44,30 @@ def remesh(dir):
         os.remove(file)
 
 
-save_output(DIR)
-remesh(NEW_DIR)
+def new_histograms(dir):
+    vertices, faces = [], []
+    for file in os.listdir(dir):
+        with open(f'{dir}/{file}') as f:
+            line = f.readlines()[1]
+            vertices.append(int(line.split()[0]))
+            faces.append(int(line.split()[0]))
+
+    # Distribution of amount of vertices after preprocessing
+    plt.hist(vertices, rwidth=0.95, bins=15)
+    plt.title('Distribution of amount of vertices')
+    plt.ylabel('frequency')
+    plt.xlabel('# of vertices')
+    plt.show()
+
+    # Distribution of amount of faces after preprocessing
+    plt.hist(faces, rwidth=0.95, bins=15)
+    plt.title('Distribution of amount of faces')
+    plt.ylabel('frequency')
+    plt.xlabel('# of faces')
+    plt.show()
+
+
+# save_output(DIR)
+# subsampling(NEW_DIR)
+new_histograms(NEW_DIR)
+
