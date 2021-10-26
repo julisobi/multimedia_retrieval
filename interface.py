1a229f6cd5d4ad94ecb0fb7ae9031fecc41b5411
AlissaVermast <alissavermast@gmail.com>
2021-10-26 21:13:53 +0200

    button to go back to the start and try another mesh works (there is a weird bug that only appears sometimes but it's not a application-breaking bug)

--- interface.py
+++ interface.py
@@ -1,3 +1,5 @@
+import pywebio
+import time
 import trimesh
 from pywebio.input import *
 from pywebio.output import *
@@ -5,6 +7,7 @@ from os import listdir
 from os.path import isfile, join
 import random
 import pandas as pd
+
 import distances as dist
 from read_data import normalization_tool, diameter2
 import numpy as np
@@ -29,51 +32,72 @@ def calculate_distances(vector, df, distance):
     return distances
 
 
-def interface():
+def start_interface():
+    #while True:
     put_text("This is the interface for the Multimedia Retrieval project")
-    option = radio("Select a mesh:", ["Choose mesh from existing", "Upload your own mesh"])
-    if option == "Choose mesh from existing":
-        category = select("Select category:", categories)
-        path = f"{DIR}/{category}"
-        files = [f for f in listdir(path) if isfile(join(path, f)) & f.endswith(".off")]
-        random_file = random.choice(files)
-        files.append("Random")
-        file = select("Select file:", files)
-        if file == "Random":
-            file = random_file
-        file_path = f"{path}\{file}"
-        df = pd.read_excel(excel_file, index_col=0)
-        row = df.loc[(df['path'] == file_path)]
-        headers = list(row)[1:]
-        values = df.loc[(df['path'] == file_path)].values[0][1:]
-        put_table([headers, values])
-        dist = calculate_distances(values, df, "cos")
-        dist.sort(key=lambda tup: tup[0])
-        put_table([('Distance', 'Path')] + dist)
+    put_button("Restart and try another mesh", onclick=lambda: restart(), color='success', outline=True)
+    interface()
+    pywebio.session.hold()
 
-    else:
-        file = file_upload("Select a file:")
-        f = open('uploads/' + file['filename'], 'wb')
-        f.write(file['content'])
 
-        mesh = trimesh.load('uploads/' + file['filename'], force='mesh')
-        mesh, eigenvectors, eigenvalues = normalization_tool(mesh)
-        area = float(mesh.area)
-        volume = abs(float(mesh.volume))
-        compactness = float((mesh.area ** 3) / (36 * np.pi * (volume ** 2)))
-        diameter = float(diameter2(mesh))
-        eccentricity = eigenvalues[0] / eigenvalues[2]
-        values = [area, volume, compactness, diameter, eccentricity]
-        headers = ['area', 'volume', 'compactness', 'diameter', 'eccentricity']
-        put_table([headers, values])
+def interface():
+    with use_scope('B'):
+        option = radio("Select a mesh:", ["Choose mesh from existing", "Upload your own mesh"])
+        if option == "Choose mesh from existing":
+            category = select("Select category:", categories)
+            path = f"{DIR}/{category}"
+            files = [f for f in listdir(path) if isfile(join(path, f)) & f.endswith(".off")]
+            random_file = random.choice(files)
+            files.append("Random")
+            file = select("Select file:", files)
+            if file == "Random":
+                file = random_file
+            file_path = f"{path}\{file}"
+            df = pd.read_excel(excel_file, index_col=0)
+            row = df.loc[(df['path'] == file_path)]
+            headers = list(row)[1:]
+            values = df.loc[(df['path'] == file_path)].values[0][1:]
 
-        df = pd.read_excel(excel_file, index_col=0)
-        dist = calculate_distances(values, df, "cos")
-        dist.sort(key=lambda tup: tup[0])
-        put_table([('Distance', 'Path')] + dist)
+            pywebio.output.clear('B')
 
+            put_table([headers, values])
+            dist = calculate_distances(values, df, "cos")
+            dist.sort(key=lambda tup: tup[0])
+            put_table([('Distance', 'Path')] + dist)
+            while not next:
+                time.sleep(1)
+
+        else:
+            file = file_upload("Select a file:")
+            f = open('uploads/' + file['filename'], 'wb')
+            f.write(file['content'])
+
+            mesh = trimesh.load('uploads/' + file['filename'], force='mesh')
+            mesh, eigenvectors, eigenvalues = normalization_tool(mesh)
+            area = float(mesh.area)
+            volume = abs(float(mesh.volume))
+            compactness = float((mesh.area ** 3) / (36 * np.pi * (volume ** 2)))
+            diameter = float(diameter2(mesh))
+            eccentricity = eigenvalues[0] / eigenvalues[2]
+            values = [area, volume, compactness, diameter, eccentricity]
+            headers = ['area', 'volume', 'compactness', 'diameter', 'eccentricity']
+
+            pywebio.output.clear('B')
+
+            put_table([headers, values])
+            df = pd.read_excel(excel_file, index_col=0)
+            dist = calculate_distances(values, df, "cos")
+            dist.sort(key=lambda tup: tup[0])
+            put_table([('Distance', 'Path')] + dist)
+
+
+def restart():
+    print('restart')
+    pywebio.output.clear('B')
+    interface()
 
 if __name__ == '__main__':
-    interface()
+    #pywebio.start_server(start_interface(), debug=True)
+    start_interface()
 
 
