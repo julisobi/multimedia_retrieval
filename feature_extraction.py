f12a90f4633dc446a29b602b6bfe122e2d8b2fd3
Julita Sobiczewska <44268786+julisobi@users.noreply.github.com>
2021-10-27 07:42:27 +0200

    Add files via upload
    speed improved

--- feature_extraction.py
+++ feature_extraction.py
@@ -14,40 +14,53 @@ def distance(p1, p2):
 
 
 def a3(points): 
-    random_points = random.sample(list(points), 3)
-    ba = np.array(random_points[0]) - np.array(random_points[1])
-    bc = np.array(random_points[2]) - np.array(random_points[1])
+    # random_points = random.sample(list(points), 3)
+    # ba = np.array(random_points[0]) - np.array(random_points[1])
+    # bc = np.array(random_points[2]) - np.array(random_points[1])
+    random.shuffle(points)
+    idx = random.sample(range(len(points)), 3)
+    ba = np.array(points[idx[0]]) - np.array(points[idx[1]])
+    bc = np.array(points[idx[2]]) - np.array(points[idx[1]])
     cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
     angle = np.arccos(cosine_angle)
     return np.degrees(angle)
 
 
 def d1(points, center):
-    random_p = random.choice(list(points))
+    random.shuffle(points)
+    idx = random.choice(range(len(points)))
+    # random_p = random.choice(list(points))
+    random_p = points[idx]
     return distance(np.array(center), np.array(random_p))
 
 
 def d2(points):
-    random_points = random.sample(list(points), 2)
-    dist = distance(np.array(random_points[0]), np.array(random_points[1]))
+    random.shuffle(points)
+    idx = random.sample(range(len(points)), 2)
+    # random_points = random.sample(list(points), 2)
+    dist = distance(np.array(points[idx[0]]), np.array(points[idx[1]]))
     return dist
 
 
 def d3(points):
-    random_points = random.sample(list(points), 3)
-    dist1 = distance(np.array(random_points[0]), np.array(random_points[1]))
-    dist2 = distance(np.array(random_points[1]), np.array(random_points[2]))
-    dist3 = distance(np.array(random_points[0]), np.array(random_points[2]))
+    # random_points = random.sample(list(points), 3)
+    random.shuffle(points)
+    idx = random.sample(range(len(points)), 3)
+    dist1 = distance(np.array(points[idx[0]]), np.array(points[idx[1]]))
+    dist2 = distance(np.array(points[idx[1]]), np.array(points[idx[2]]))
+    dist3 = distance(np.array(points[idx[0]]), np.array(points[idx[2]]))
     s = (dist1 + dist2 + dist3) / 2
     area = math.sqrt(s * (s - dist1) * (s - dist2) * (s - dist3))
     return area
 
 
 def d4(points):
-    random_points = random.sample(list(points), 4)
-    ad = np.array(random_points[0]) - np.array(random_points[3])
-    bd = np.array(random_points[1]) - np.array(random_points[3])
-    cd = np.array(random_points[2]) - np.array(random_points[3])
+    # random_points = random.sample(list(points), 4)
+    random.shuffle(points)
+    idx = random.sample(range(len(points)), 4)
+    ad = np.array(points[idx[0]]) - np.array(points[idx[3]])
+    bd = np.array(points[idx[1]]) - np.array(points[idx[3]])
+    cd = np.array(points[idx[2]]) - np.array(points[idx[3]])
     val = np.array([ad, bd, cd])
     abs_val = abs(np.linalg.det(val))
     volume = abs_val / 6
@@ -57,35 +70,40 @@ def d4(points):
 
 def save_values_a3(mesh, num_points):
     a3_val = []
+    points = list(mesh.vertices)
     for i in range(num_points):
-        a3_val.append(a3(mesh.vertices))
+        a3_val.append(a3(points))
     return a3_val
 
 
 def save_values_d1(mesh, num_points):
-    center = mesh.center_mass
     d1_val = []
+    center = mesh.center_mass
+    points = list(mesh.vertices)
     for i in range(num_points):
-        d1_val.append(d1(mesh.vertices, center))
+        d1_val.append(d1(points, center))
     return d1_val
 
 
 def save_values_d2(mesh, num_points):
     d2_val = []
+    points = list(mesh.vertices)
     for i in range(num_points):
-        d2_val.append(d2(mesh.vertices))
+        d2_val.append(d2(points))
     return d2_val
 
 
 def save_values_d3(mesh, num_points):
     d3_val = []
+    points = list(mesh.vertices)
     for i in range(num_points):
-        d3_val.append(d3(mesh.vertices))
+        d3_val.append(d3(points))
     return d3_val
 
 
 def save_values_d4(mesh, num_points):
     d4_val = []
+    points = list(mesh.vertices)
     for i in range(num_points):
-        d4_val.append(d4(mesh.vertices))
+        d4_val.append(d4(points))
     return d4_val
