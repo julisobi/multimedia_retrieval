c96440beb5430d512ba6aad271ad6d4561ced633
Julita Sobiczewska <44268786+julisobi@users.noreply.github.com>
2021-11-10 09:56:24 +0100

    Add files via upload

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,7 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score
 import pandas as pd
 from distances import ann
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def evaluate_meshes(excel_file):
@@ -15,14 +16,17 @@ def evaluate_meshes(excel_file):
         distances = ann(df, file, 10, 20)
         categories = [tup[1].split("/")[1] for tup in distances]
         y_pred = [1 if cat == category else 0 for cat in categories]
-        y_true = [1] * 20
-        acc = accuracy_score(y_true, y_pred)
-        prec = precision_score(y_true, y_pred)
-        recc = recall_score(y_true, y_pred)
+        TP = y_pred.count(1)
+        FP = y_pred.count(0)
+        FN = 20 - TP
+        TN = 360 - FP
+        acc = (TP + TN) / (TP + TN + FP + FN)
+        prec = TP / (TP + FP)
+        rec = TP / (TP + FN)
         total_acc += acc
         total_prec += prec
-        total_recc += recc
-        acc_prec_recc_cat_list.append((acc, prec, recc, category))
+        total_recc += rec
+        acc_prec_recc_cat_list.append((acc, prec, rec, category))
     print(f"Airplane accuracy, precision, recall: {sum([e[0] for e in acc_prec_recc_cat_list if e[3] == 'Airplane']) / 20}, {sum([e[1] for e in acc_prec_recc_cat_list if e[3] == 'Airplane']) / 20}, {sum([e[2] for e in acc_prec_recc_cat_list if e[3] == 'Airplane']) / 20}")
     print(f"Ant accuracy: {sum([e[0] for e in acc_prec_recc_cat_list if e[3] == 'Ant']) / 20}, {sum([e[1] for e in acc_prec_recc_cat_list if e[3] == 'Ant']) / 20}, {sum([e[2] for e in acc_prec_recc_cat_list if e[3] == 'Ant']) / 20}")
     print(f"Armadillo accuracy: {sum([e[0] for e in acc_prec_recc_cat_list if e[3] == 'Armadillo']) / 20}, {sum([e[1] for e in acc_prec_recc_cat_list if e[3] == 'Armadillo']) / 20}, {sum([e[2] for e in acc_prec_recc_cat_list if e[3] == 'Armadillo']) / 20}")
@@ -47,6 +51,39 @@ def evaluate_meshes(excel_file):
     print(f"Total precision: {total_prec / 380}")
     print(f"Total recall: {total_recc / 380}")
 
+    return acc_prec_recc_cat_list
 
 
-evaluate_meshes("normalized.xlsx")
+def histograms(results_list):
+    categories = ['Airplane', 'Ant', 'Armadillo', 'Bearing', 'Bird', 'Bust', 'Chair', 'Cup', 'Fish', 'FourLeg',
+                  'Glasses', 'Hand', 'Human', 'Mech', 'Octopus', 'Plier', 'Table', 'Teddy', 'Vase']
+    acc, prec, rec = [], [], []
+    results = []
+    for cat in categories:
+        results.append((cat, sum([e[0] for e in results_list if e[3] == cat]) / 20, sum([e[1] for e in results_list if e[3] == cat]) / 20, sum([e[2] for e in results_list if e[3] == cat]) / 20))
+        acc.append(sum([e[0] for e in results_list if e[3] == cat]) / 20)
+        prec.append(sum([e[1] for e in results_list if e[3] == cat]) / 20)
+        rec.append(sum([e[2] for e in results_list if e[3] == cat]) / 20)
+    results.sort(key=lambda tup: tup[1], reverse=True)
+
+    x = np.arange(len(categories))
+    width = 0.2
+
+    acc_sorted = [tup[1] for tup in results]
+    prec_sorted = [tup[2] for tup in results]
+    rec_sorted = [tup[3] for tup in results]
+    cat_sorted = [tup[0] for tup in results]
+    fig, ax = plt.subplots()
+    ax.bar(x - width, acc_sorted, width, label='accuracy')
+    ax.bar(x, prec_sorted, width, label='precision')
+    ax.bar(x + width, rec_sorted, width, label='recall')
+    ax.set_ylabel('Results')
+    ax.set_xticks(x)
+    ax.set_xticklabels(cat_sorted)
+    plt.xticks(rotation=90)
+    ax.legend()
+    plt.show()
+
+
+# evaluate_meshes("normalized.xlsx")
+histograms(evaluate_meshes("normalized.xlsx"))
